36026fca569e8ffce09d176e1ff501e59d4a1dbc
Garrett Howard <garrett@mersh.com>
2023-07-30 03:13:13 -0500

    Fix critical mistakes in steam_id.rs docs

--- src/steam_id.rs
+++ src/steam_id.rs
@@ -3,8 +3,6 @@ use core::fmt;
 use serde::Deserialize;
 use serde_derive::{Deserialize, Serialize};
 
-use crate::Steam;
-
 /// Represents a SteamID64 type which is used to uniquely identify users on the Steam platform.
 /// SteamID64 is a 64-bit unsigned integer.
 /// For more information on SteamID and SteamID64, refer to the official Steam documentation:
@@ -13,7 +11,7 @@ use crate::Steam;
 /// # Examples
 ///
 /// ```
-/// use steam-rs::steam_id::SteamId;
+/// use steam_rs::steam_id::SteamId;
 ///
 /// let steam_id = SteamId::new(76561197960287930);
 /// println!("SteamId: {}", steam_id);
@@ -23,17 +21,17 @@ use crate::Steam;
 ///
 /// You can convert a `SteamId` to a `u64` using the `into()` method:
 /// ```
-/// use steam-rs::steam_id::SteamId;
+/// use steam_rs::steam_id::SteamId;
 ///
 /// let steam_id: SteamId = 76561197960287930.into();
-/// println!("SteamId as u64: {}", steam_id.into());
+/// println!("SteamId as u64: {}", steam_id);
 /// ```
 ///
 /// # Formatting
 ///
 /// The `SteamId` can be formatted as a string for display purposes:
 /// ```
-/// use steam-rs::steam_id::SteamId;
+/// use steam_rs::steam_id::SteamId;
 ///
 /// let steam_id = SteamId::new(76561197960287930);
 /// println!("Formatted SteamId: {}", steam_id.to_string());
@@ -43,7 +41,7 @@ use crate::Steam;
 ///
 /// You can parse a `SteamId` from a string representation:
 /// ```
-/// use steam-rs::steam_id::SteamId;
+/// use steam_rs::steam_id::SteamId;
 ///
 /// let steam_id_str = "76561197960287930";
 /// let steam_id = SteamId::from_str(steam_id_str).unwrap();
