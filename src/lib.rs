c6ba149a2f5731af33d02fb94407b818599e0d7c
Garrett Howard <garrett@mersh.com>
2023-07-30 03:03:59 -0500

    Provide documentation for struct `SteamId` and lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
-//! # steam-rs
+//! # steam-rs: Safe bindings for the Steam Web API
 //!
-//! The `steam-rs` crate provides convenient Rust bindings for the Steam Web API.
+//! This crate provides safe and convenient Rust bindings for the Steam Web API.
+//! 
+//! ### Warning!
+//! This crate is currently a work in progress, so please expect breaking changes and instability. Please be careful when using this! **This is not production ready!**
 
 pub mod player_service;
 pub mod published_file_service;
