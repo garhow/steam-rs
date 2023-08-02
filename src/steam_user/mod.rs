d2877bed62f4470b98be2fd87e818426a3f7c5f3
Garrett Howard <garrett@mersh.com>
2023-08-02 04:55:07 -0500

    Add documentation for steam_user/mod.rs

--- src/steam_user/mod.rs
+++ src/steam_user/mod.rs
@@ -1,3 +1,21 @@
+//! # Implements the ISteamUser interface
+//! 
+//! **Note:** This implementation is incomplete!
+//! The following endpoints are currently unimplemented:
+//! 
+//! - CheckAppOwnership (requires publisher key)
+//! - GetAppPriceInfo (requires publisher key)
+//! - GetDeletedSteamIDs (requires publisher key)
+//! - GetPlayerBans
+//! - GetPublisherAppOwnership (requires publisher key)
+//! - GetPublisherAppOwnershipChanges (requires publisher key)
+//! - GetUserGroupList
+//! - GrantPackage (requires publisher key)
+//! - RevokePackage (requires publisher key)
+//! 
+//! Endpoints that require a publisher key are not likely to be
+//! implemented in the near future, as they cannot be tested by developers.
+
 const INTERFACE: &str = "ISteamUser";
 
 pub mod get_friend_list;
