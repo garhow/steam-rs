290a3873f8378cada4efd441788f1130cc36ad8a
Garrett Howard <garrett@mersh.com>
2023-08-02 06:42:33 -0500

    Add documentation to steam_user/resolve_vanity_url.rs

--- src/steam_user/resolve_vanity_url.rs
+++ src/steam_user/resolve_vanity_url.rs
@@ -1,3 +1,5 @@
+//! Implements the `ResolveVanityURL` endpoint
+
 use serde::{Deserialize, Serialize};
 use serde_json::{Value, from_value};
 
@@ -20,16 +22,43 @@ struct Wrapper {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Response {
-    message: Option<String>, // The message associated with the request status. Currently only used on resolution failures.
-    steamid: Option<String>, // The 64 bit Steam ID the vanity URL resolves to. Not returned on resolution failures.
-    success: u8 // The status of the request. 1 if successful, 42 if there was no match.
+    /// The message associated with the request status. Currently only used on resolution failures.
+    pub message: Option<String>,
+
+    /// The 64 bit Steam ID the vanity URL resolves to. Not returned on resolution failures.
+    pub steamid: Option<String>,
+
+    /// The status of the request. 1 if successful, 42 if there was no match.
+    pub success: u8
 }
 
 impl Steam {
+    /// Resolve vanity URL parts to a 64-bit ID.
+    ///
+    /// # Arguments
+    ///
+    /// * `vanity_url` - The user's vanity URL that you would like to retrieve a steam ID for, e.g. <http://steamcommunity.com/id/gabelogannewell> would use "gabelogannewell"
+    /// * `url_type` - The type of vanity URL.
+    ///     * 1 (default): Individual profile
+    ///     * 2: Group,
+    ///     * 3: Official game group
+    /// 
+    /// # Example
+    /// 
+    /// ```
+    ///     // Creates new `Steam` instance using the environment variable `STEAM_API_KEY`.
+    ///     let steam = Steam::new(&std::env::var("STEAM_API_KEY").expect("Missing an API key"));
+    /// 
+    ///     // Resolves the vanity URL `gabelogannewell`.
+    ///     let resolved_vanity_url = steam.resolve_vanity_url("gabelogannewell", None).await.unwrap();
+    /// 
+    ///     // Prints the 64-bit ID of the user of the vanity URL
+    ///     println!("{:?}", resolved_vanity_url.steamid);
+    /// ```
     pub async fn resolve_vanity_url(
         &self,
-        vanity_url: &str, // The vanity URL to get a SteamID for
-        url_type: Option<i32> // The type of vanity URL. 1 (default): Individual profile, 2: Group, 3: Official game group
+        vanity_url: &str,
+        url_type: Option<i32>
     ) -> Result<Response, SteamUserError> {
         let query = vec![
             format!("?key={}", &self.api_key),
