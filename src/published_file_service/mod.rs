e5ff1ef41bad4236c45c7ebc0119ee37a4c91eb2
Garrett Howard <garrett@mersh.com>
2023-08-05 18:26:33 -0500

    Update module `published_file_service` - Add documentation - Refactor the module

--- src/published_file_service/mod.rs
+++ src/published_file_service/mod.rs
@@ -1,3 +1,31 @@
-pub const INTERFACE: &str = "IPublishedFileService";
+//! # Implements the `IPublishedFileService` interface
+//!
+//! **Note:** This implementation is incomplete!
+//! The following endpoints are currently unimplemented:
+//!
+//! - CanSubscribe (undocumented)
+//! - Delete (undocumented)
+//! - GetDetails
+//! - GetSubSectionData
+//! - GetUserFileCount
+//! - GetUserFiles
+//! - GetUserVoteSummary
+//! - Publish (undocumented)
+//! - RefreshVotingQueue (requires publisher key)
+//! - SetDeveloperMetadata (requires publisher key)
+//! - Subscribe (requires publisher key)
+//! - Unsubscribe (requires publisher key)
+//! - Update (requires publisher key)
+//! - UpdateAppUGCBan (requires publisher key)
+//! - UpdateBanStatus (requires publisher key)
+//! - UpdateIncompatibleStatus (requires publisher key)
+//! - UpdateKeyValueTags (requires publisher key)
+//! - UpdateTags (requires publisher key)
+//! - Vote (requires publisher key)
+//!
+//! Endpoints that require a publisher key are not likely to be
+//! implemented in the near future, as they cannot be tested by developers.
 
 pub mod query_files;
+
+const INTERFACE: &str = "IPublishedFileService";
--- src/published_file_service/query_files.rs
+++ src/published_file_service/query_files.rs
@@ -1,11 +1,14 @@
+//! Implements the `QueryFiles` endpoint
+
 use core::fmt;
 
 use serde::{Deserialize, Serialize};
 
-use crate::{Steam,
-    errors::{PublishedFileServiceError, ErrorHandle},
+use crate::{
+    errors::{ErrorHandle, PublishedFileServiceError},
     macros::{do_http, optional_argument},
-    BASE, steam_id::{SteamId, de_steamid_from_str},
+    steam_id::{de_steamid_from_str, SteamId},
+    Steam, BASE,
 };
 
 use super::INTERFACE;
@@ -36,7 +39,7 @@ pub enum PublishedFileQueryType {
     RankedByLifetimePlaytimeSessions,
     RankedByInappropriateContentRating,
     RankedByBanContentCheck,
-    RankedByLastUpdatedDate
+    RankedByLastUpdatedDate,
 }
 
 impl fmt::Display for PublishedFileQueryType {
@@ -63,7 +66,7 @@ impl fmt::Display for PublishedFileQueryType {
             PublishedFileQueryType::RankedByLifetimePlaytimeSessions => 18,
             PublishedFileQueryType::RankedByInappropriateContentRating => 19,
             PublishedFileQueryType::RankedByBanContentCheck => 20,
-            PublishedFileQueryType::RankedByLastUpdatedDate => 21
+            PublishedFileQueryType::RankedByLastUpdatedDate => 21,
         };
         write!(f, "{}", value)
     }
@@ -71,27 +74,66 @@ impl fmt::Display for PublishedFileQueryType {
 
 #[derive(Debug)]
 pub enum PublishedFileInfoMatchingFileType {
+    /// Items.
     Items,
+
+    /// A collection of Workshop items.
     Collections,
+
+    /// Artwork.
     Art,
+
+    /// Videos.
     Videos,
+
+    /// Screenshots
     Screenshots,
+
+    /// Items that can be put inside a collection.
     CollectionEligible,
+
+    /// Unused.
     Games,
+
+    /// Unused.
     Software,
+
+    /// Unused.
     Concepts,
+
+    /// Unused.
     GreenlightItems,
+
+    /// Guides.
     AllGuides,
+
+    /// Steam web guide.
     WebGuides,
+
+    /// Application integrated guide.
     IntegratedGuides,
+
     UsableInGame,
+
+    /// Workshop merchandise meant to be voted on for the purpose of being sold
     Merch,
+
+    /// Steam Controller bindings.
     ControllerBindings,
+
+    /// Used internally.
     SteamworksAccessInvites,
+
+    /// Workshop items that can be sold in-game.
     ItemsMtx,
+    
+    /// Workshop items that can be used right away by the user.
     ItemsReadyToUse,
+    
     WorkshopShowcase,
-    GameManagedItems
+
+    /// Managed completely by the game, not the user, and not shown on the web.
+    GameManagedItems,
 }
 
 impl fmt::Display for PublishedFileInfoMatchingFileType {
@@ -117,7 +159,7 @@ impl fmt::Display for PublishedFileInfoMatchingFileType {
             PublishedFileInfoMatchingFileType::ItemsMtx => 17,
             PublishedFileInfoMatchingFileType::ItemsReadyToUse => 18,
             PublishedFileInfoMatchingFileType::WorkshopShowcase => 19,
-            PublishedFileInfoMatchingFileType::GameManagedItems => 20
+            PublishedFileInfoMatchingFileType::GameManagedItems => 20,
         };
         write!(f, "{}", value)
     }
@@ -129,28 +171,28 @@ pub struct Preview {
     preview_id: String,
     #[serde(rename = "sortorder")]
     sort_order: u16,
-    url:  Option<String>,
+    url: Option<String>,
     size: Option<u32>,
     #[serde(rename = "filename")]
     file_name: Option<String>,
-    preview_type: u8
+    preview_type: u8,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Tag {
     tag: String,
-    display_name: String
+    display_name: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct VoteData {
-    score: f32
+    score: f32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct PlaytimeStats {
     playtime_seconds: String,
-    num_sessions: String
+    num_sessions: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -209,22 +251,56 @@ pub struct File {
     revision_change_number: String,
     revision: u32,
     available_revisions: Vec<u32>,
-    ban_text_check_result: u32
+    ban_text_check_result: u32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct PublishedFiles {
     total: u64,
     #[serde(rename = "publishedfiledetails")]
-    published_file_details: Vec<File>
+    published_file_details: Vec<File>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Response {
-    response: PublishedFiles
+    response: PublishedFiles,
 }
 
 impl Steam {
+    /// Performs a search query for published files
+    ///
+    /// # Arguments
+    /// 
+    /// * `query_type` - Type of the query, see [PublishedFileQueryType](crate::published_file_service::query_files::PublishedFileQueryType).
+    /// * `page` - Current page. Currently there is an upper limit of 1000.
+    /// * `cursor` - Cursor to paginate through the results (set to '*' for the first request). Prefer this over using the page parameter, as it will allow you to do deep pagination. When used, the page parameter will be ignored. Use the "next_cursor" value returned in the response to set up the next query to get the next set of results.
+    /// * `numperpage` - The number of results, per page to return.
+    /// * `creator_app_id` - App that created the files.
+    /// * `app_id` - App that consumes the files.
+    /// * `required_tags` - Tags to match on. See `match_all_tags` parameter.
+    /// * `excluded_tags` - Tags that must NOT be present on a published file to satisfy the query.
+    /// * `match_all_tags` - If true, then items must have all the tags specified, otherwise they must have at least one of the tags.
+    /// * `required_flags` - Required flags that must be set on any returned items.
+    /// * `omitted_flags` - Flags that must not be set on any returned items
+    /// * `search_text` - Text to match in the item's title or description.
+    /// * `file_type` - 
+    /// * `child_published_file_id` - Find all items that reference the given item.
+    /// * `days` - If `query_type` is [RankedByTrend](crate::published_file_service::query_files::PublishedFileQueryType::RankedByTrend), then this is the number of days to get votes for \[1,7\].
+    /// * `include_recent_votes_only` - If `query_type` is [RankedByTrend](crate::published_file_service::query_files::PublishedFileQueryType::RankedByTrend), then limit result set just to items that have votes within the day range given.
+    /// * `cache_max_age_seconds` - Allow stale data to be returned for the specified number of seconds.
+    /// * `language` - Language to search in and also what gets returned. Defaults to English.
+    /// * `required_kv_tags` - Required key-value tags to match on.
+    /// * `total_only` - If true, only return the total number of files that satisfy this query.
+    /// * `ids_only` - If true, only return the published file ids of files that satisfy this query.
+    /// * `return_vote_data` - Return vote data.
+    /// * `return_tags` - Return tags in the file details.
+    /// * `return_kv_tags` - Return key-value tags in the file details.
+    /// * `return_previews` - Return preview image and video details in the file details.
+    /// * `return_children` - Return child item ids in the file details.
+    /// * `return_short_description` - Populate the short_description field instead of file_description.
+    /// * `return_for_sale_data` - Return pricing information, if applicable.
+    /// * `return_metadata` - Populate the metadata.
+    /// * `return_playtime_stats` Return playtime stats for the specified number of days before today.
     pub async fn query_files(
         &self,
         query_type: PublishedFileQueryType,
@@ -256,7 +332,7 @@ impl Steam {
         return_short_description: bool,
         return_for_sale_data: bool,
         return_metadata: Option<bool>,
-        return_playtime_stats: u32
+        return_playtime_stats: u32,
     ) -> Result<PublishedFiles, PublishedFileServiceError> {
         let query = vec![
             format!("?key={}", &self.api_key),
@@ -292,9 +368,21 @@ impl Steam {
             optional_argument!(return_metadata),
         ];
 
-        let url = format!("{}/{}/{}/v{}/{}", BASE, INTERFACE, ENDPOINT, VERSION, query.concat());
+        let url = format!(
+            "{}/{}/{}/v{}/{}",
+            BASE,
+            INTERFACE,
+            ENDPOINT,
+            VERSION,
+            query.concat()
+        );
 
-        let response = do_http!(url, Response, ErrorHandle, PublishedFileServiceError::QueryFiles);
+        let response = do_http!(
+            url,
+            Response,
+            ErrorHandle,
+            PublishedFileServiceError::QueryFiles
+        );
 
         Ok(response.response)
     }
