5462d2e0d1844e4d466c935fb55d249798aa54d9
Garrett Howard <garrett@mersh.com>
2024-02-10 12:26:41 -0600

    Print return data in  test

--- tests/steam_economy.rs
+++ tests/steam_economy.rs
@@ -10,7 +10,7 @@ pub fn get_asset_class_info() {
             .get_asset_class_info(440, None, 1, 211447708, None)
             .await
             .unwrap();
-        println!("{data:?}");
+        println!("{data:?}")
     });
 }
 
@@ -19,6 +19,10 @@ pub fn get_asset_prices() {
     async_test!(async {
         let steam = Steam::new(&std::env::var("STEAM_API_KEY").expect("Missing an API key"));
 
-        steam.get_asset_prices(440, None, None).await.unwrap();
+        let data = steam
+            .get_asset_prices(440, None, None)
+            .await
+            .unwrap();
+        println!("{data:?}")
     });
 }
