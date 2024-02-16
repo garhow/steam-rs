5831bc59a779a72ac0cde3ccaae5d0f6cf00856f
Garrett Howard <garrett@mersh.com>
2024-02-16 05:22:55 -0600

    Make `STEAM_GAME_TICKET` variable optional for testing

--- tests/steam_user_auth.rs
+++ tests/steam_user_auth.rs
@@ -7,16 +7,18 @@ pub fn authenticate_user_ticket() {
     async_test!(async {
         let steam = Steam::new(&std::env::var("STEAM_API_KEY").expect("Missing an API key"));
         let app_id = 12900; // Audiosurf
-        let ticket = &std::env::var("STEAM_GAME_TICKET").expect("Ticket should be provided");
+        let ticket = &std::env::var("STEAM_GAME_TICKET").unwrap_or(String::new());
 
-        let query = steam
-            .authenticate_user_ticket(
-                app_id,
-                ticket,
-            )
-            .await
-            .unwrap();
-
-        println!("{:?}", query);
+        if !ticket.is_empty() {
+            assert!(
+                steam
+                    .authenticate_user_ticket(
+                        app_id,
+                        ticket,
+                    )
+                    .await
+                    .is_ok()
+            );
+        }
     });
 }
